b67322b4fd4e4fe36658481c3d063a608940189d
Afougeres <49587647+Afougeres@users.noreply.github.com>
2019-04-13 14:25:26 +0200

    TP-git
    J'ai pas tout compris pour pushez l'historique meme si j'ai effectué tout le TP

---
+++ Versionning-TP1/Readme.txt
@@ -0,0 +1,2 @@
+Titre du projet : Versionning-TP1
+Auteur : Antoine Fougeres BTS A1 2019
---
+++ Versionning-TP1/src/application.bash
@@ -0,0 +1,6 @@
+### Application.bash - 201x - Fougeres ###
+version="2"
+build_hour="11H00"
+
+echo "Build${build_hour}"
+echo "version${version}"
